893a7e0563376e99fddae7c0f67ca5653fb0641a
CaolineVivian <anyangov49@gmail.com>
2020-12-09 11:52:47 +0300

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range (0, 10):
-    for j in range (0, 10):
+for i in range(0, 10):
+    for j in range(0, 10):
         if i != j:
             print("{}{}".format(i, j), end=" ")
 print("\n")
-

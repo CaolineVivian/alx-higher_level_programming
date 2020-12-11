283f5f6d99aa28d473eb077056ea29cbbe927b00
CaolineVivian <anyangov49@gmail.com>
2020-12-11 10:51:07 +0300

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97,123):
-    i = chr(i)
-    if i != 'q'and i != 'e':
-        print("{}".format(i), end="")
+    if i != 'e'and i != 'q':
+        print("{}".format(chr(i)), end="")

1dbe819211da75a4601649bdfd781fb46a0b9875
CaolineVivian <anyangov49@gmail.com>
2020-12-15 22:51:38 +0300

    updates

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    args = len(sys.argv)
-    if args == 1:
-        print("{}".format(args - 1))
-    elif args > 1:
-        total = 0
-        for x in range(1, args):
-            total += int(sys.argv[x])
-        print ("{}".format(total))
+    from sys import argv
+
+    if len(argv) == 1:
+        print('{:d}'.format(len(argv)-1))
+
+    elif len(argv) > 1:
+        sum = 0
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print('{:d}'.format(sum))
